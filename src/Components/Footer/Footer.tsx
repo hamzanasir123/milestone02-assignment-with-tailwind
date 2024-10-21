1e99701b599131a228d184f9cb924763743d1b24
Hamza Nasir <hk435981@gmail.com>
2024-10-21 11:37:37 +0500

    changes

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -5,8 +5,7 @@ function Footer() {
   return (
     <>
     <footer
-    className='w-full text-center bg-orange-200 
-    '
+    className='w-full text-center bg-orange-200'
     >
         <p>Hamza Nasir @ all copyrights reserved</p>
     </footer>
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Link from 'next/link'
 import React from 'react'
 
@@ -6,9 +7,9 @@ function Header() {
     <>
     <nav className='grid sm:grid-cols-2 sm:h-10 bg-orange-200'>
         <div>
-        <Link href={"#"} className='m-2 ml-10 font-serif font-black '>HAMZA NASIR</Link>
+        <Link href={"/"} className='m-2 ml-10 font-serif font-black '>HAMZA NASIR</Link>
         </div>
-        <div className=''>
+        <div>
         <Link href={"/"} className='m-2 font-serif font-black '>HOME</Link>
         <Link href={"/About"} className='m-2 font-serif font-black '>ABOUT</Link>
         <Link href={"/Contact"} className='m-2 font-serif font-black '>CONTACT</Link>
