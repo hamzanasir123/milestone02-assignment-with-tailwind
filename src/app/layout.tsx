39c28767644e0616c1aaa055f272de4e226e2a02
Hamza Nasir <hk435981@gmail.com>
2024-10-21 11:26:55 +0500

    Changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from "@/Components/Header/Header";
 import "./globals.css";
 import Footer from "@/Components/Footer/Footer";
+
 export default function RootLayout({
   children,
 }: Readonly<{
